f8f7a5d28df239e649e150fb3e289e0770cf9889
lensonlovescode <lensonisinparis@gmail.com>
2025-09-08 22:34:11 +0300

    First APIs corrections

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,13 +2,13 @@ import DBClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AppController {
-  static getStatus(res) {
+  static getStatus(req, res) {
     if (DBClient.isAlive() && redisClient.isAlive()) {
       res.status(200).json({ redis: true, db: true });
     }
   }
 
-  static getStats(res) {
+  static getStats(req, res) {
     const users = DBClient.nbUsers();
     const files = DBClient.nbFiles();
     res.status(200).json({ users, files });
